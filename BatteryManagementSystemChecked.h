0ca0edd12c650fb5347b34a6fa2c4e5397ec7db5
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-11 20:58:37 +0530

    Create BatteryManagementSystemChecked.h

---
+++ BatteryManagementSystemChecked.h
@@ -0,0 +1,8 @@
+//Header file with function prototypes
+
+int batteryTemperatureCheck(float temperature);
+int batterySOCCheck(float soc);
+int batteryChargerateCheck(float chargeRate);
+int printBatteryTemperatureCheckStatus(float temperature);
+int printBatterySOCCheckStatus(float soc);
+int printBatteryChargeRateCheckStatus(float chargeRate);
