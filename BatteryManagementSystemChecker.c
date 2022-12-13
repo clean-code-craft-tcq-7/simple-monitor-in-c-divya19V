11a95ffbdbb3ea1c315b5a7e6798ade2ea9a3000
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-13 20:15:13 +0530

    Added test cases

--- BatteryManagementSystemChecker.c
+++ BatteryManagementSystemChecker.c
@@ -12,10 +12,20 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 }
 
 int main() {
+  //Positive cases
   assert(batteryIsOk(25, 70, 0.7));
+  assert(batteryIsOk(45, 20, 0.1));
+  assert(batteryIsOk(0, 21, -9));
+  
+  //Negative cases
+  assert(!batteryIsOk(50, 85, 0));
+  assert(!batteryIsOk(100, 18, 0.9));
+  assert(!batteryIsOk(-12, 0, 11));
+  assert(!batteryIsOk(100, -10, 20));
   
   printBatteryTemperatureCheckStatus(50);
   printBatterySOCCheckStatus(85);
   printBatteryChargeRateCheckStatus(0);
-  assert(!batteryIsOk(50, 85, 0));
+ 
+ 
 }
