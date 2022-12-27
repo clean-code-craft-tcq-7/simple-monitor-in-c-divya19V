22bb5789bc9097f6059cefeee1c2c461463a9d93
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-27 12:31:33 +0530

    Updated BatteryManagementSystemChecked.h file

--- BatteryManagementSystemChecked.h
+++ BatteryManagementSystemChecked.h
@@ -3,6 +3,4 @@
 int batteryTemperatureCheck(float temperature);
 int batterySOCCheck(float soc);
 int batteryChargerateCheck(float chargeRate);
-void printBatteryTemperatureCheckStatus(float temperature);
-void printBatterySOCCheckStatus(float soc);
-void printBatteryChargeRateCheckStatus(float chargeRate);
+
