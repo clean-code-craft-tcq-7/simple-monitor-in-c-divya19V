59fc6dc83c5e8a30ec13922733955e553105fe6e
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-27 13:02:47 +0530

    Updated BatteryManagementSystemSupportFile.c file with constants & warnings

--- BatteryManagementSystemSupportFile.c
+++ BatteryManagementSystemSupportFile.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
 
-int batteryTemperatureCheck(float temperature) {
-  if (temperature < 0 || temperature > 45) 
-     return 0;
+#define TEMP_MIN_LIMIT 0
+#define TEMP_MAX_LIMIT 45
+#define SOC_MIN_LIMIT 20
+#define SOC_MAX_LIMIT 80
+#define CHARGERATE_MAX_LIMIT 0.8
+
+#define TEMP_TOLERANCE_UPRLIMIT (TEMP_MAX_LIMIT*0.05)
+#define SOC_TOLERANCE_UPRLIMIT (SOC_MAX_LIMIT*0.05)
+#define CHARGERATE_TOLERANCE_UPRLIMIT (CHARGERATE_MAX_LIMIT*0.05)
+
+#define TRUE 1
+#define FALSE 0
+
+bool TEMPERATUREWARNINGSET 0
+bool SOCWARNINGSET 0
+bool CHARGERATEWARNINGSET 0
+
+  
+bool batteryTemperatureCheckForError(float temperature) {
+  if (temperature < TEMP_MIN_LIMIT || temperature > TEMP_MAX_LIMIT) 
+     return FALSE;
   else 
-    return 1;
+    return TRUE;
 }
 
-int batterySOCCheck(float soc) {
-  if(soc < 20 || soc > 80) 
-    return 0;
+bool batteryTemperatureCheckForWarnings(float temperature) {
+  if ((temperature <= TEMP_MIN_LIMIT+TEMP_TOLERANCE_UPRLIMIT && temperature >= TEMP_MIN_LIMIT) || 
+      (temperature >= TEMP_MAX_LIMIT-TEMP_TOLERANCE_UPRLIMIT && temperature <= TEMP_MAX_LIMIT))
+    TEMPERATUREWARNINGSET = TRUE;
+}
+
+bool batterySOCCheckForError(float soc) {
+  if(soc < SOC_MIN_LIMIT || soc > SOC_MAX_LIMIT) 
+    return FALSE;
   else
-    return 1;
+    return TRUE;
 }
 
-int batteryChargerateCheck(float chargeRate) {
-  if(chargeRate > 0.8)
-    return 0;
+bool batterySOCCheckForWarnings(float soc) {
+  if((soc >= SOC_MIN_LIMIT && soc <= SOC_MIN_LIMIT+SOC_TOLERANCE_UPRLIMIT) ||
+    (soc <= SOC_MAX_LIMIT && soc >= SOC_MAX_LIMIT-SOC_TOLERANCE_UPRLIMIT))
+    SOC_TOLERANCE_UPRLIMIT = TRUE;
+}
+
+
+bool batteryChargerateCheckForError(float chargeRate) {
+  if(chargeRate > CHARGERATE_MAX_LIMIT)
+    return FALSE;
   else
-    return 1;
+    return TRUE;
 }
 
-void printBatteryTemperatureCheckStatus(float temperature){
-  if(!batteryTemperatureCheck(temperature))
-     printf("Temperature out of range!\n"); 
-}
-
-void printBatterySOCCheckStatus(float soc){
-  if(!batterySOCCheck(soc))
-    printf("State of Charge out of range!\n");
-}
-
-void printBatteryChargeRateCheckStatus(float chargeRate){
-  if(!batteryChargerateCheck(chargeRate))
-    printf("Charge Rate out of range!\n");
+bool batteryChargerateCheckForWarnings(float chargeRate) {
+  if(chargeRate <= CHARGERATE_MAX_LIMIT && chargeRate >= CHARGERATE_MAX_LIMIT-CHARGERATE_TOLERANCE_UPRLIMIT)
+   CHARGERATEWARNINGSET = TRUE;
 }
