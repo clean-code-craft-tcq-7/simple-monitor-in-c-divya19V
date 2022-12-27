6e8544d2e0ecbccd55b021caf1675b3fd0d12e8a
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-27 19:30:53 +0530

    Create BMSSOCSupportFile.c

---
+++ BMSSOCSupportFile.c
@@ -0,0 +1,25 @@
+#include "BatteryManagementSystemSupportFile.c"
+
+bool batterySOCCheckForError(float soc) {
+  if(soc < SOC_MIN_LIMIT) {
+     printStatementsIndex = 1;
+    return FALSE;
+  }
+  else if (soc > SOC_MAX_LIMIT) {
+     printStatementsIndex = 5;
+    return FALSE;
+  }
+  else {
+     printStatementsIndex = 3;
+    return TRUE;
+  }
+}
+
+bool batterySOCCheckForWarnings(float soc) {
+  if(soc >= SOC_MIN_LIMIT && soc <= SOC_MIN_LIMIT+SOC_TOLERANCE_UPRLIMIT) 
+      printStatementsIndex = 2;
+  else if(soc <= SOC_MAX_LIMIT && soc >= SOC_MAX_LIMIT-SOC_TOLERANCE_UPRLIMIT)
+      printStatementsIndex = 4;
+  else
+      printStatementsIndex = 3;
+}
