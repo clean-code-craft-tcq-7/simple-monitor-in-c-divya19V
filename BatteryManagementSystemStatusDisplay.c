ff3027ac5a02030aa8824281a723edea5591dbb1
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-28 19:11:56 +0530

    Updated BatteryManagementSystemStatusDisplay.c

--- BatteryManagementSystemStatusDisplay.c
+++ BatteryManagementSystemStatusDisplay.c
@@ -2,56 +2,32 @@
 #include "BatteryManagementSystemStatusDisplay.h"
 #include "BatteryManagementSystemSupportFile.h"
 
-/*
-void printBatteryTemperatureCheckStatus(float temperature){
-  if(!batteryTemperatureCheck(temperature))
-     printf("Temperature out of range!\n"); 
-}
-
-void printBatterySOCCheckStatus(float soc){
-  if(!batterySOCCheck(soc))
-    printf("State of Charge out of range!\n");
-}
-
-void printBatteryChargeRateCheckStatus(float chargeRate){
-  if(!batteryChargerateCheck(chargeRate))
-    printf("Charge Rate out of range!\n");
-}
-*/
-
-int FindTheMessageIndex()
-{
-    //array of [3][5]
-    row = printStatementsIndex;
-    col = printStatementsIndex
-}
-
+//Function to print message based on the parameter value
 void printBatteryParametersStatus(float temperature, float soc, float chargeRate) {
    bool returnVal= FALSE;
    unsigned int inputParam = 0;
    unsigned int paramValues = 0;
-   printf ("You want to see the status for how many parameters?");
+   printf ("You want to see the status for how many parameters? \n Input should be in btw 1 to 3:");
    scanf ("%d", &inputParam);
    if (inputParam > 0) {
       printf("Which parameter status would you like to view?");
       printf("1: temperature\t2: SOC\t3: ChareRate\t4: Exit");
-      for (int index=1;index<=inputParam;input++) {
-          scanf("%d", paramValues);
-     
-          if(paramValues == 4)
-            return 0;
+      scanf("%d", paramValues);
+      for (int index=1;index<=paramValues;input++) {
+          if(paramValues >= 4)
+            return;
           else if(paramValues == 1) {
             returnVal = batteryTemperatureCheck(temperature);
-            
+            printf("%s", printStatements[printStatementsIndex1.row][printStatementsIndex1.col]);
           }else if(paramValues == 2){
-          }else {//(paramValues == 3)
+            returnVal = batterySOCCheck(soc);
+            printf("%s", printStatements[printStatementsIndex1.row][printStatementsIndex1.col]);
+          }else {//(paramValues == 3) 
+            returnVal = batteryChargerateCheck(chargeRate);
+            printf("%s", printStatements[printStatementsIndex1.row][printStatementsIndex1.col]);
           }
        }
-   }
- 
-   
-   returnVal = batterySOCCheck(soc);
-   returnVal = batteryChargerateCheck(chargeRate);
+    }
   
    }
 }
