2c7f285f9ad27f2d7155a4f269455735c41d9d71
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-28 19:04:49 +0530

    Update BMSTempSupportFile.c

--- BMSTempSupportFile.c
+++ BMSTempSupportFile.c
@@ -1,22 +1,30 @@
 #include "BatteryManagementSystemSupportFile.h"
 
 bool batteryTemperatureCheck(float temperature) {
+  printStatementsIndex printStatementsIndex1;
+  
   if (temperature < TEMP_MIN_LIMIT) {
-      printStatementsIndex = 0;
-      return FALSE;
+    printStatementsIndex1.row = 0;
+    printStatementsIndex1.col = 0;      
+    return FALSE;
   }
   else if (temperature > TEMP_MAX_LIMIT) {  
-     printStatementsIndex = 4;
+     printStatementsIndex1.row = 0;
+     printStatementsIndex1.col = 4; 
      return FALSE;
   }
   else {
-    if (temperature <= TEMP_MIN_LIMIT+TEMP_TOLERANCE_UPRLIMIT && temperature >= TEMP_MIN_LIMIT)
-       printStatementsIndex = 1;  
-    else if (temperature >= TEMP_MAX_LIMIT-TEMP_TOLERANCE_UPRLIMIT && temperature <= TEMP_MAX_LIMIT)
-      printStatementsIndex = 3;
-    else 
+    if (temperature <= TEMP_MIN_LIMIT+TEMP_TOLERANCE_UPRLIMIT && temperature >= TEMP_MIN_LIMIT) [
+       printStatementsIndex1.row = 0;
+       printStatementsIndex1.col = 1;
+    }else if (temperature >= TEMP_MAX_LIMIT-TEMP_TOLERANCE_UPRLIMIT && temperature <= TEMP_MAX_LIMIT) {
+      printStatementsIndex1.row = 0;
+      printStatementsIndex1.col = 3;
+    }else {
       printStatementsIndex = 2;
-    
-    return TRUE;
+      printStatementsIndex1.row = 0;
+      printStatementsIndex1.col = 2;
+    }
+      return TRUE;
   }
 }
