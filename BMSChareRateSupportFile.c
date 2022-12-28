a58f0b0856f0bcd803e97c2f950940ce9bb442d2
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-28 12:41:06 +0530

    Update BMSChareRateSupportFile.c

--- BMSChareRateSupportFile.c
+++ BMSChareRateSupportFile.c
@@ -1,18 +1,16 @@
 #include "BatteryManagementSystemSupportFile.c"
 
-bool batteryChargerateCheckForError(float chargeRate) {
+bool batteryChargerateCheck(float chargeRate) {
   if(chargeRate > CHARGERATE_MAX_LIMIT) {
     printStatementsIndex = 5;
     return FALSE;
   } else {
-    printStatementsIndex = 4;
+    if(chargeRate <= CHARGERATE_MAX_LIMIT && chargeRate >= CHARGERATE_MAX_LIMIT-CHARGERATE_TOLERANCE_UPRLIMIT)
+      printStatementsIndex = 5;
+    else
+      printStatementsIndex = 4;
+    
     return TRUE;
   }
 }
-
-bool batteryChargerateCheckForWarnings(float chargeRate) {
-  if(chargeRate <= CHARGERATE_MAX_LIMIT && chargeRate >= CHARGERATE_MAX_LIMIT-CHARGERATE_TOLERANCE_UPRLIMIT)
-    printStatementsIndex = 5;
-  else
-    printStatementsIndex = 4;
-}
+  
